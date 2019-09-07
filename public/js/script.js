cd3eb7c4f75591b5c772aa10448e0c8afb3dbd34
Vipul Chodankar <vipul72c@gmail.com>
2019-09-07 14:40:33 +0530

    Added done & remove functionality

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,12 @@
-console.log("Connected.");
+$("li").click(function() {
+  $(this).toggleClass("completed");
+});
+
+$("span").click(function(event) {
+  $(this)
+    .parent()
+    .fadeOut(500, function() {
+      $(this).remove();
+    });
+  event.stopPropagation();
+});
